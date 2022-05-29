46f82e82778696f3cf5cbea16f787e6b38ce5086
Antongiacomo Polimeno <antongiacomo.polimeno@unimi.it>
2022-05-30 01:18:32 +0200

    Aggiunti i colori, modificato il flusso

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,47 @@
 mod gioco;
+use colored::Colorize;
+
 
 fn lanciamo() -> u32 {
-    let dadi = vec![6, 6, 6] ;
-    let (punteggio, _risultati) = gioco::lanciatore::Lanciatore::new(dadi).lancia();
-    return punteggio
+    let dadi = vec![6, 6, 6];
+    let (punteggio, risultati) =  gioco::lanciatore::Lanciatore::new(dadi).lancia();
+    println!("{:?}", risultati);
+    return punteggio;
 }
 
+fn color_string(string: &String, red: bool) -> String {
+    if red {
+        return format!("{}", string).red().to_string();
+    }
+
+    return format!("{}", string).green().to_string();
+}
 fn main() {
     let mut pti_g1 = 0;
     let mut pti_g2 = 0;
-
-    for _i in 1..=10000{
+    let mut c = 0;
+    while {
+        print!("Lancia Giocatore 1: ");
         pti_g1 += lanciamo();
+        print!("Lancia Giocatore 2: ");
         pti_g2 += lanciamo();
-        // thread::sleep(Duration::from_millis(100));
-    }
 
-    println!("Punteggio Antongiacomo: {}", pti_g1);
-    println!("Punteggio Giorgio: {}", pti_g2);
+        println!();
+        c += 1;
+        (c < 20) | (pti_g1 != pti_g2)
+    } {}
 
-    if pti_g1 > pti_g2 {
-        println!("Antongiacomo ha vinto!");
-    } else if pti_g1 < pti_g2 {
-        println!("Giorgio ha vinto!");
-    } else {
-        println!("Pareggio!");
-    }
+    // thread::sleep(Duration::from_millis(100));
+
+    println!("Partite Giocate: {}", c);
+    println!(
+        "{0: <24}: {1: <20}",
+        "Punteggio Antongiacomo",
+        color_string(&pti_g1.to_string(), pti_g2 >= pti_g1)
+    );
+    println!(
+        "{0: <24}: {1: <20}",
+        "Punteggio Giorgio",
+        color_string(&pti_g2.to_string(), pti_g1 >= pti_g2)
+    );
 }
